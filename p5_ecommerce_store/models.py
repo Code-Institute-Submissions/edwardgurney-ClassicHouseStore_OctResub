9451a285188930e99818eff4f94613a9a2243a62
edwardgurney <edwardgurney1979@gmail.com>
2022-03-27 11:02:38 +0000

    Added product and category models

--- p5_ecommerce_store/models.py
+++ p5_ecommerce_store/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    artist = models.CharField(max_length=200)
+    label = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateTimeField(auto_now=False)
+    # rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = CloudinaryField('image', default='placeholder')
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    audio_file = models.FileField(blank=True, null=True)
+    audio_link = models.FileField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
