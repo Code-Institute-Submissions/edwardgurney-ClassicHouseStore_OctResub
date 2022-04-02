91240b045666cc3cf689e8b31d96ae77095aebde
edwardgurney <edwardgurney1979@gmail.com>
2022-04-02 14:37:33 +0000

    Added basket.html

--- p5_ecommerce_store/views.py
+++ p5_ecommerce_store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from .models import Product
 
 class StoreFrontView(generic.ListView):
@@ -9,3 +9,11 @@ class StoreFrontView(generic.ListView):
 class ProductDetailView(generic.DetailView):
     model = Product
     template_name = 'p5_ecommerce_store/product_detail.html'
+
+class BasketView(View):
+    template_name = 'p5_ecommerce_store/basket.html'
+
+    # def get():
+    
+def basket_view(request):
+    return render(request, 'p5_ecommerce_store/basket.html')
