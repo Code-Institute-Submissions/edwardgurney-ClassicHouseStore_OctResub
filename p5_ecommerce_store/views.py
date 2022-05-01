cc38b5a810e0e60e9ba134f832c37f4778d5c5d2
edwardgurney <edwardgurney1979@gmail.com>
2022-05-01 10:36:19 +0000

    Fixing general error of static paths as not finding them

--- p5_ecommerce_store/views.py
+++ p5_ecommerce_store/views.py
@@ -197,6 +197,7 @@ def payment_view(request, pk):
 	context = {}
 	context['client_secret'] = create_payment_intent(request, pk)
 	context['bag_id'] = order.bag.id
+	context['order'] = order
 	context['return_url'] = reverse_lazy('checkout_complete', kwargs={'pk':pk})
 	#context['return_url'] = request.build_absolute_uri(reverse_lazy('checkout_complete', kwargs={'pk':pk}))
 	context['public_stripe'] = settings.STRIPE_PUBLIC_KEY
