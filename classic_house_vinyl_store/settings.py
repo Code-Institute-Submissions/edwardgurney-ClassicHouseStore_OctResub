e46848b19212851073ad74176d67dde1bb525c42
edwardgurney <edwardgurney1979@gmail.com>
2022-05-26 17:56:26 +0000

    Final push with some commented code from settings removed

--- classic_house_vinyl_store/settings.py
+++ classic_house_vinyl_store/settings.py
@@ -123,17 +123,6 @@ EMAIL_USE_SSL = True
 
 WSGI_APPLICATION = 'classic_house_vinyl_store.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
